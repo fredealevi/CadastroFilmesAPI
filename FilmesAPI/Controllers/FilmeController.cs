18c6e6419f015cc5be7fe144c738afac8cd5db34
Frederico Lima Alevi <fredealevi@hotmail.com>
2022-05-13 12:41:46 -0300

    Formação Alura ASP.NET Core REST APIs - Módulo 3 - Status: Concluído

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public FilmeController(FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        [HttpPost]
+        public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
+        {
+            Filme filme = _mapper.Map<Filme>(filmeDto);
+         
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(ListarFilmes), new { id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> ListarFilmes()
+        {
+            return _context.Filmes;
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null)
+            {
+                ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+              
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult AtualizarFilme (int id, [FromBody] UpdateFilmeDto filmeNovoDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(filmeNovoDto, filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeletaFilme (int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+
+
+}
---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Diretor { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório.")]
+        [StringLength(30, ErrorMessage = "Máximo 15 caractéres. ")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório.")]
+        [Range(1, 600)]
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; }
+    }
+}
